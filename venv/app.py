357d5f607983fae8f910b88e2168bf6b324ee366
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-03-16 15:11:34 +0530

    Hello

--- venv/app.py
+++ venv/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from functools import wraps
 
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "ProjectBuiltByTeamTerminators"
@@ -13,13 +14,38 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+# Login Manager
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 class Student(UserMixin, db.Model):
     __tablename__ = "students"
     sid = db.Column(db.Integer, primary_key=True)
-    
     name = db.Column(db.Text, unique=True, nullable=False)
     email = db.Column(db.Text, unique=False, nullable=False)
     password = db.Column(db.Text, nullable=False)
+    mobile = db.Column(db.Text, nullable=True)
+    course = db.Column(db.Text, nullable=True)
+    branch = db.Column(db.Text, nullable=True)
+    father_name = db.Column(db.Text, nullable=True)
+    caste = db.Column(db.Text, nullable=True)
+    eamcet_rank = db.Column(db.Text, nullable=True)
+    bank = db.Column(db.Text, nullable=True)
+    aadhar = db.Column(db.Text, nullable=True)
+    ration_card = db.Column(db.Text, nullable=True)
+    address = db.Column(db.Text, nullable=True)
+    father_name = db.Column(db.Text, nullable=True)
+    mother_name = db.Column(db.Text, nullable=True)
+    father_mobile = db.Column(db.Text, nullable=True)
+    mother_mobile = db.Column(db.Text, nullable=True)
+    annual_income = db.Column(db.Text, nullable=True)
+    father_mail = db.Column(db.Text, nullable=True)
+    mother_mail = db.Column(db.Text, nullable=True)
+    father_occupation = db.Column(db.Text, nullable=True)
+    mother_occupation = db.Column(db.Text, nullable=True)
+    profile = db.Column(db.Text, nullable=True)
+    nationality = db.Column(db.Text, nullable=True)
 
 class Employee(UserMixin, db.Model):
     __tablename__ = "employees"
@@ -28,11 +54,14 @@ class Employee(UserMixin, db.Model):
     name = db.Column(db.Text, unique=True, nullable=False)
     email = db.Column(db.Text, unique=False, nullable=False)
     password = db.Column(db.Text, unique=False)
+    department = db.Column(db.Text, nullable=True)
+    qualification = db.Column(db.Text, nullable=True)
+    gender = db.Column(db.Text, nullable=True)
 
-class Subject(db.Model):
-    __tablename__ = "subjects"
-    sub_id = db.Column(db.Integer, primary_key=True)
-    sub_name = db.Column(db.Text, unique=True, nullable=False)
+# class Subject(db.Model):
+#     __tablename__ = "subjects"
+#     sub_id = db.Column(db.Integer, primary_key=True)
+#     sub_name = db.Column(db.Text, unique=True, nullable=False)
     
 # class Notes(db.Model):
 #     __tablename__ = "notes"
@@ -43,23 +72,29 @@ class Subject(db.Model):
 
 # db.create_all()
 
-def is_admin():
-    users_id = [1, 2, 3]
-    if current_user.id not in users_id:
-        return False
-    else:
+def is_emp():
+    if current_user.eid:
         return True
+    else:
+        return False
 
-def admin_only(f):
+def emp_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         try:
-            if not is_admin():
+            if not is_emp():
                 return abort(403)
             return f(*args, **kwargs)      
         except:
               return abort(403)
     return decorated_function
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return Student.query.get(user_id)
+    except:
+        return Employee.query.get(user_id)
 
 @app.route("/")
 def home():
@@ -107,6 +142,15 @@ def attendance():
 @app.route("/add_attendance")
 def add_attendance():
     return render_template("add_attendance.html")
+
+@app.route("/add-student")
+def add_student():
+    return ""
+
+@app.route("/logout")
+def logout():
+    logout_user(current_user)
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000, debug=True)
