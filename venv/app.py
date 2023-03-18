9b24980e62e58e5ed78a1e7f61360391492d1d7c
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-03-18 11:18:24 +0530

    SC

--- venv/app.py
+++ venv/app.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin, LoginManager, login_user, logout_user, login_
 from functools import wraps
 from io import BytesIO
 from PyPDF2 import PdfReader
-
+from datetime import date
 
 app = Flask(__name__)
 
@@ -21,6 +21,8 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+# Global variables
+mark_list = []
 class Student(UserMixin, db.Model):
     __tablename__ = "students"
     sid = db.Column(db.Integer, primary_key=True)
@@ -48,6 +50,7 @@ class Student(UserMixin, db.Model):
     mother_occupation = db.Column(db.Text, nullable=True)
     profile = db.Column(db.Text, nullable=True)
     nationality = db.Column(db.Text, nullable=True)
+    attendance_percent = db.Column(db.Integer, nullable=True)
 
 class Employee(UserMixin, db.Model):
     __tablename__ = "employees"
@@ -60,10 +63,6 @@ class Employee(UserMixin, db.Model):
     qualification = db.Column(db.Text, nullable=True)
     gender = db.Column(db.Text, nullable=True)
 
-# class Subject(db.Model):
-#     __tablename__ = "subjects"
-#     sub_id = db.Column(db.Integer, primary_key=True)
-#     sub_name = db.Column(db.Text, unique=True, nullable=False)
 
 class Notes(db.Model):
     __tablename__ = "notes"
@@ -80,6 +79,12 @@ def is_emp():
         return True
     else:
         return False
+    
+def is_stud():
+    if current_user.sid:
+        return True
+    else:
+        return False
 
 def emp_only(f):
     @wraps(f)
@@ -92,29 +97,66 @@ def emp_only(f):
               return abort(403)
     return decorated_function
 
+def stud_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            if not is_stud():
+                return abort(403)
+            return f(*args, **kwargs)      
+        except:
+              return abort(403)
+    return decorated_function
+
 @login_manager.user_loader
 def load_user(user_id):
-    try:
+    if not is_emp():
         return Student.query.get(user_id)
-    except:
+    else:
         return Employee.query.get(user_id)
 
 # Home route
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", logged_in=current_user.is_authenticated)
 
 # Attendance section
 @app.route("/attendance")
+@stud_only
 def attendance():
-    return render_template("attendance.html")
+    return render_template("attendance.html", logged_in=current_user.is_authenticated)
 
-@app.route("/add_attendance")
+@app.route("/add_attendance", methods=["GET", "POST"])
+@emp_only
 def add_attendance():
-    return render_template("add_attendance.html")
+    today = date.today()
+    def total_days():
+        start_date = date(2023, 3, 14)
+        return int((today-start_date).days)
+    
+    students = Student.query.all()
+    if request.method=="POST":
+        for student in students:
+            is_present = request.form.get(str(student.sid))
+            if student.attendance_percent:
+                no_of_days_present = (student.attendance_percent * total_days()-1) // 100
+            else:
+                no_of_days_present = 0
+            
+            if is_present=="1":
+                no_of_days_present += 1
+            student.attendance_percent = int((no_of_days_present / total_days())*100)
+            db.session.commit()
+            mark_list.append(today)
+    if date.today() in mark_list:
+        is_completed = True
+    else:
+        is_completed = False
+    return render_template("add_attendance.html", students=students, today=today, is_completed=is_completed, logged_in=current_user.is_authenticated)
 
 # Notes routes
 @app.route("/add-notes", methods=["GET", "POST"])
+@emp_only
 def add_notes():
     if request.method == "POST":
         new_notes = Notes(
@@ -124,12 +166,12 @@ def add_notes():
         db.session.add(new_notes)
         db.session.commit()
         return redirect(url_for("add_notes"))
-    return render_template("add_notes.html")
+    return render_template("add_notes.html", logged_in=current_user.is_authenticated)
 
 @app.route("/notes")
 def notes():
     notes = Notes.query.all()
-    return render_template("notes.html", notes=notes)
+    return render_template("notes.html", notes=notes, logged_in=current_user.is_authenticated)
 
 @app.route("/download-notes/<int:nid>")
 def download_notes(nid):
@@ -204,13 +246,13 @@ def student_form():
         db.session.commit()
         return redirect(url_for("student_form"))
 
-    return render_template("student_form.html")
+    return render_template("student_form.html", logged_in=current_user.is_authenticated)
 
 @app.route("/stu", methods=["POST"])
 def stu():
     if request.method == "POST":
         email = request.form.get("email")
-        user = Student.query.filter_by(email=email).form()
+        user = Student.query.filter_by(email=email).first()
         if user:
             pwd = request.form.get("pwd")
             if user.password == pwd:
@@ -222,8 +264,9 @@ def stu():
         return redirect(url_for("login"))
 
 @app.route("/profile")
+@stud_only
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", logged_in=current_user.is_authenticated)
 
 # Employee
 @app.route("/emp-form", methods=["GET", "POST"])
@@ -242,7 +285,7 @@ def emp_form():
         db.session.commit()
         return redirect(url_for("emp_form"))
 
-    return render_template("emp_form.html")
+    return render_template("emp_form.html", logged_in=current_user.is_authenticated)
 
 @app.route("/emp", methods=["POST"])
 def emp():
@@ -260,10 +303,11 @@ def emp():
 
         flash("User not registered with email!")
         return redirect(url_for("login"))
-
+    return redirect(url_for("home"))
     # return render_template("login.html", logged_in=current_user.is_authenticated)
 
 @app.route("/logout")
+@login_required
 def logout():
     logout_user(current_user)
     return redirect(url_for("home"))
